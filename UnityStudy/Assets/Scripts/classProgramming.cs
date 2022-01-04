8813363c15ba288d1f2230984a32fb156e808ac0
unknown <ydlwnsj25@gmail.com>
2022-01-04 21:09:20 +0900

    for No MonoBehaviour Class

---
+++ UnityStudy/Assets/Scripts/classProgramming.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classProgramming 
+{
+    void Update()
+    {
+        Debug.Log("This is classProgramming Update Function");
+    }
+}
